30e6fd9ee48b51160a3d618e3cca868f9bffcdfe
dohee_kang <doheee.kang@gmail.com>
2019-09-05 15:23:16 +0900

    [Feat] Attach google map in map screen
    * using react-native-maps
    * api key in the app.json

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default function MapScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Map Screen</Text>
-    </View>
+    <MapView
+      style={{ flex: 1 }}
+      initialRegion={{
+        latitude: 37.2852555,
+        longitude: 127.0466507,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+      }}
+    />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-});
 
 MapScreen.navigationOptions = {
   header: null
