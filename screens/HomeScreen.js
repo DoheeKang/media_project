0057580fad0a14550e700a9ca5ff19efb665ac44
DoheeKang <doheee.kang@gmail.com>
2019-09-06 15:19:38 +0900

    [Feat] Get weather data and show home screen
    * using openweather api
    * install axios

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, Text } from 'react-native';
-
+import axios from 'axios';
 export default function HomeScreen() {
+  const [weather, setWeather] = useState({
+    main: { temp: 100 },
+    weather: [{ main: 'Sunny' }]
+  });
+  useEffect(() => {
+    axios
+      .get(
+        'http://api.openweathermap.org/data/2.5/weather?q=Seoul&APPID=9285169024c6d787301b9060e3bb2ed3'
+      )
+      .then(res => {
+        setWeather(res.data);
+      });
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text>날씨: {weather.weather[0].main}</Text>
+      <Text>기온: {Math.ceil(weather.main.temp - 273.15)}℃</Text>
+      <Text>습도: {weather.main.humidity}</Text>
     </View>
   );
 }
