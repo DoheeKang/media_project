d3a274903b2e3bec2d3b8ec8266e59f4ceedd029
DoheeKang <doheee.kang@gmail.com>
2019-09-08 03:01:24 +0900

    [Feat] Render marker on specific position
    * Just temp data
    * It will use when show nursing room

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,7 +1,12 @@
 import React from 'react';
-import MapView from 'react-native-maps';
+import { View, Text } from 'react-native';
+import MapView, { Callout, Marker } from 'react-native-maps';
 
 export default function MapScreen() {
+  const latlng = {
+    latitude: 37.2852555,
+    longitude: 127.0466507
+  };
   return (
     <MapView
       style={{ flex: 1 }}
@@ -11,7 +16,16 @@ export default function MapScreen() {
         latitudeDelta: 0.0922,
         longitudeDelta: 0.0421
       }}
-    />
+    >
+      <Marker coordinate={latlng}>
+        <Callout>
+          <View>
+            <Text>장소 이름</Text>
+            <Text>구체적인 장소</Text>
+          </View>
+        </Callout>
+      </Marker>
+    </MapView>
   );
 }
 
