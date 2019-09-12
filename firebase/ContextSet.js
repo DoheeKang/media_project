df0dd0cdbf2949b82f26230b73aaffea7530522f
김범규 <gimbeomgyu@gimbeomgyuui-MacBookPro.local>
2019-09-12 15:56:44 +0900

    Add firebase Context

---
+++ firebase/ContextSet.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const ContextSet = {
+	DataContext: React.createContext(),
+	EmailContext: React.createContext(),
+	PasswordContext: React.createContext(),
+};
+
+export default ContextSet;
