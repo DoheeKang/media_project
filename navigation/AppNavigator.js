01f9a264720254f31483aea56dc227920a99db6a
DoheeKang <doheee.kang@gmail.com>
2019-09-15 00:49:53 +0900

    [Feat] Add AuthLoading and Auth to the navigator
    * initial is AuthLoading
    * navigate auth or main page depending on login

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-
+import React, { useContext } from 'react';
+import {
+  createAppContainer,
+  createStackNavigator,
+  createSwitchNavigator
+} from 'react-navigation';
+import { ActivityIndicator, View } from 'react-native';
 import MainTabNavigator from './MainTabNavigator';
+import AuthScreen from '../components/AuthScreen';
+import { ContextSet } from '../firebase';
+
+const AuthLoadingScreen = props => {
+  const [data, setData] = useContext(ContextSet.DataContext);
+
+  props.navigation.navigate(data ? 'Main' : 'Auth');
+
+  return (
+    <View>
+      <ActivityIndicator />
+    </View>
+  );
+};
+
+const AuthStack = createStackNavigator({ SignIn: AuthScreen });
 
 export default createAppContainer(
-  createSwitchNavigator({
-    // You could add another route here for authentication.
-    // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-    Main: MainTabNavigator,
-  })
+  createSwitchNavigator(
+    {
+      Main: MainTabNavigator,
+      Auth: AuthStack,
+      AuthLoading: AuthLoadingScreen
+    },
+    {
+      initialRouteName: 'AuthLoading'
+    }
+  )
 );
