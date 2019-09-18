2725c74aa034a123565f62c3dc6cd60d03e01102
DoheeKang <doheee.kang@gmail.com>
2019-09-18 16:49:05 +0900

    [Feat] Add collection 'contents' in fireStore and get it on the contentsScreen

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,16 +1,17 @@
-import firebase from "firebase/app";
-import "@firebase/auth";
-import "@firebase/firestore";
-import config from "./config";
+import firebase from 'firebase/app';
+import '@firebase/auth';
+import '@firebase/firestore';
+import config from './config';
 
 const firebaseApp = () => {
-	if (!firebase.apps.length) {
-		firebase.initializeApp(config);
-	}
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
 
-	const auth = firebase.auth();
-	const users = firebase.firestore().collection("users");
-	return [auth, users];
+  const auth = firebase.auth();
+  const users = firebase.firestore().collection('users');
+  const contents = firebase.firestore().collection('contents');
+  return [auth, users, contents];
 };
 
 export default firebaseApp;
--- screens/ContentScreen.js
+++ screens/ContentScreen.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { StyleSheet, View, Text } from 'react-native';
+import { firebaseApp } from '../firebase';
 
 export default function ContentScreen() {
+  const [auth, users, contents] = firebaseApp();
+
+  contents.get().then(function(querySnapshot) {
+    querySnapshot.forEach(function(doc) {
+      console.log(doc.id, ' => ', doc.data());
+    });
+  });
+
   return (
     <View style={styles.container}>
       <Text>Content Screen</Text>
