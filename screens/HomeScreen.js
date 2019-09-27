50c2f1df664ff500d66b67f927c7409762cdd020
DoheeKang <doheee.kang@gmail.com>
2019-09-28 01:34:33 +0900

    [Style] add icon and fit layout on homeScreen weather

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,17 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
 import ContentScreen from './ContentScreen';
 
 import { LinearGradient } from 'expo-linear-gradient';
-
 import axios from 'axios';
+
+/* Images */
+import sunnyIcon from '../assets/images/sunny.png';
+import dustIcon from '../assets/images/dust.png';
+import fineDustIcon from '../assets/images/fineDust.png';
+
+import good from '../assets/images/good.png';
 
 const weatherCases = {
   Thunderstorm: {
@@ -79,6 +85,10 @@ const weatherAPI =
 const fineDustAPI =
   'http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?sidoName=서울&pageNo=1&numOfRows=10&ServiceKey=tSa88T5QB46%2Bx3bYTLNANgFJ7yXjFRCXtNnvFbVzEQYGwu33Grfr%2Fteoy7%2FJ%2BpN3KQtla8iSX0Tjr9AIcUGf9A%3D%3D&ver=1.3&_returnType=json';
 
+const newDate = new Date();
+const date = newDate.getDate();
+const month = newDate.getMonth() + 1;
+
 export default function HomeScreen() {
   const [weather, setWeather] = useState({
     main: { temp: 100 },
@@ -110,62 +120,116 @@ export default function HomeScreen() {
     getInfo();
   }, []);
 
-  console.log('Home Screen: ', isHomeDetail);
-  if (isLoad) {
-    return (
-      <View style={styles.container}>
-        {isHomeDetail ? (
-          <></>
-        ) : (
-          <LinearGradient
-            start={{ x: 0, y: 0.5 }}
-            end={{ x: 1, y: 0.5 }}
-            colors={['#62cdaa', '#79d19b', '#90d392']}
-            style={styles.weather}
-          >
-            {/* <MaterialCommunityIcons
-            color="black"
-            size={144}
-            name={weatherCases[weather.weather[0].main].icon}
-          ></MaterialCommunityIcons> */}
-            <View>
-              <Text>오늘</Text>
-              <Text>서울 {Math.ceil(weather.main.temp - 273.15)}º</Text>
-              <Text>미세먼지 {fineDustCases[dust.pm10Grade].condition}</Text>
-              <Text>초미세먼지 {fineDustCases[dust.pm25Grade].condition}</Text>
+  return (
+    <View style={styles.container}>
+      {isHomeDetail ? (
+        <></>
+      ) : (
+        <LinearGradient
+          start={{ x: 0, y: 0.5 }}
+          end={{ x: 1, y: 0.5 }}
+          colors={['#62cdaa', '#79d19b', '#90d392']}
+          style={styles.weather}
+        >
+          {isLoad ? (
+            <View style={{ flex: 1 }}>
+              <View style={{ flex: 2 }}></View>
+              <View
+                style={{
+                  flex: 5,
+                  flexDirection: 'row',
+                  paddingLeft: 30,
+                  paddingRight: 30
+                }}
+              >
+                <View
+                  style={{
+                    flex: 1
+                  }}
+                >
+                  <Text style={styles.date}>
+                    {month}월 {date}일
+                  </Text>
+                  <View style={styles.row}>
+                    <Image
+                      source={sunnyIcon}
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <Text style={styles.text}>
+                      서울 {Math.ceil(weather.main.temp - 273.15)}º
+                    </Text>
+                  </View>
+                  <View style={styles.row}>
+                    <Image
+                      source={dustIcon}
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <Text style={styles.text}>
+                      미세먼지 {fineDustCases[dust.pm10Grade].condition}
+                    </Text>
+                  </View>
+                  <View style={styles.row}>
+                    <Image
+                      source={fineDustIcon}
+                      style={{ width: 30, height: 30 }}
+                    />
+                    <Text style={styles.text}>
+                      초미세먼지 {fineDustCases[dust.pm25Grade].condition}
+                    </Text>
+                  </View>
+                </View>
+                <View
+                  style={{
+                    flex: 1,
+                    justifyContent: 'center',
+                    alignItems: 'center'
+                  }}
+                >
+                  <Image source={good} style={{ width: 120, height: 120 }} />
+                  <Text style={styles.text}>나들이하기 좋아요!</Text>
+                </View>
+              </View>
             </View>
-            <View>
-              <Text>우우우</Text>
+          ) : (
+            <View style={styles.container}>
+              <Text>Weather Loading...</Text>
             </View>
-          </LinearGradient>
-        )}
+          )}
+        </LinearGradient>
+      )}
 
-        <View style={styles.contents}>
-          <ContentScreen
-            isHome={true}
-            setIsHomeDetail={setIsHomeDetail}
-          ></ContentScreen>
-        </View>
+      <View style={styles.contents}>
+        <ContentScreen
+          isHome={true}
+          setIsHomeDetail={setIsHomeDetail}
+        ></ContentScreen>
       </View>
-    );
-  } else {
-    return (
-      <View style={styles.container}>
-        <Text>Weather Loading...</Text>
-      </View>
-    );
-  }
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  row: {
+    flexDirection: 'row'
+  },
+  date: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingHorizontal: 6,
+    paddingVertical: 15
+  },
+  text: {
+    color: 'white',
+    fontSize: 15,
+    paddingHorizontal: 6,
+    paddingVertical: 6
+  },
   weather: {
-    flex: 4,
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 4
   },
   contents: {
     flex: 5,
