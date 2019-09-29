d4f3d9d98ba75ab9211279e0e446f66587e181f8
DoheeKang <doheee.kang@gmail.com>
2019-09-30 07:06:28 +0900

    [Style] Markup on InfoPage

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -5,15 +5,18 @@ import {
   Text,
   Dimensions,
   TouchableHighlight,
-  Alert
+  Alert,
+  Image
 } from 'react-native';
+import Colors from '../constants/Colors';
+import logoIcon from '../assets/images/logo.png';
 import { Avatar, Divider, Card } from 'react-native-elements';
 import { LinearGradient } from 'expo-linear-gradient';
 import { firebaseApp, ContextSet } from '../firebase';
 import { MaterialIcons } from '@expo/vector-icons';
 
 const width = Dimensions.get('window').width;
-const gray = '#f2f2f2';
+const { textColor, gray, lightGray } = Colors;
 
 export default function InfoScreen(props) {
   const [auth, users, contents] = firebaseApp();
@@ -82,23 +85,52 @@ export default function InfoScreen(props) {
         start={{ x: 0, y: 0.5 }}
         end={{ x: 1, y: 0.5 }}
         colors={['#62cdaa', '#79d19b', '#90d392']}
-        style={{ flex: 1 }}
-      ></LinearGradient>
+        style={styles.header}
+      >
+        <Image source={logoIcon} style={{ width: 70, height: 70 }} />
+      </LinearGradient>
       <View style={{ flex: 7 }}>
         <View style={{ flex: 1 }}>
           <TouchableHighlight onPress={checkLogout} underlayColor="white">
-            <Text style={{ textAlign: 'right' }}>로그아웃</Text>
+            <Text
+              style={[
+                styles.text,
+                {
+                  color: lightGray,
+                  textAlign: 'right',
+                  paddingRight: 10,
+                  paddingTop: 5
+                }
+              ]}
+            >
+              로그아웃
+            </Text>
           </TouchableHighlight>
           <View style={{ justifyContent: 'center', alignItems: 'center' }}>
             <Avatar size="large" rounded icon={{ name: 'person' }} />
-            <Text>{data['userName']}님</Text>
+            <Text style={[styles.text, { paddingTop: 5 }]}>
+              {data['userName']}님
+            </Text>
           </View>
         </View>
+
         <View style={{ width, height: 20, backgroundColor: gray }}></View>
+
         <View style={{ flex: 1 }}>
-          <Text>내 정보</Text>
+          <Text
+            style={[
+              styles.text,
+              {
+                flex: 1,
+                paddingLeft: 15,
+                textAlignVertical: 'center'
+              }
+            ]}
+          >
+            내 정보
+          </Text>
           <Divider style={{ height: 3, backgroundColor: gray }} />
-          <View style={{ flex: 1, flexDirection: 'row' }}>
+          <View style={{ flex: 3, flexDirection: 'row' }}>
             <View
               style={{
                 flex: 2,
@@ -107,8 +139,8 @@ export default function InfoScreen(props) {
                 paddingVertical: 10
               }}
             >
-              <Text style={{ paddingVertical: 10 }}>E-MAIL</Text>
-              <Text>닉네임</Text>
+              <Text style={[styles.text, { paddingVertical: 10 }]}>E-MAIL</Text>
+              <Text style={styles.text}>닉네임</Text>
             </View>
             <View
               style={{
@@ -118,18 +150,26 @@ export default function InfoScreen(props) {
                 marginVertical: 10
               }}
             >
-              <Text style={{ paddingVertical: 10 }}>{data['email']}</Text>
-              <Text>{data['userName']}</Text>
+              <Text style={[styles.text, { paddingVertical: 10 }]}>
+                {data['email']}
+              </Text>
+              <Text style={styles.text}>{data['userName']}</Text>
             </View>
           </View>
         </View>
+
         <View style={{ width, height: 20, backgroundColor: gray }}></View>
+
         <View style={{ flex: 2 }}>
-          <MaterialIcons size={30} name="bookmark"></MaterialIcons>
-          <Text>즐겨찾기</Text>
+          <View style={{ flex: 1, flexDirection: 'row', alignItems: 'center' }}>
+            <MaterialIcons size={30} name="bookmark"></MaterialIcons>
+            <Text style={[styles.text, { textAlignVertical: 'center' }]}>
+              즐겨찾기
+            </Text>
+          </View>
           <Divider style={{ height: 3, backgroundColor: gray }} />
+          <View style={{ flex: 5 }}></View>
         </View>
-        {/* <View>{likeList}</View> */}
       </View>
     </View>
   ) : (
@@ -141,10 +181,19 @@ const styles = StyleSheet.create({
   container: {
     flex: 1
   },
+  header: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
   content: {
     width: 100,
     borderRadius: 30,
     overflow: 'hidden'
+  },
+  text: {
+    fontFamily: 'BMDOHYEON',
+    color: textColor
   }
 });
 
