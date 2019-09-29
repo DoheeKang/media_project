8c29e653cead282bf3477f68f114cb230ea838b8
DoheeKang <doheee.kang@gmail.com>
2019-09-29 22:28:58 +0900

    [Feat] Add logout button on the infoPage

--- screens/InfoScreen.js
+++ screens/InfoScreen.js
@@ -1,6 +1,13 @@
-import React, { useEffect, useContext } from 'react';
-import { View, StyleSheet, Text, Dimensions } from 'react-native';
-import { Avatar, Divider } from 'react-native-elements';
+import React, { useState, useEffect, useContext } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Dimensions,
+  TouchableHighlight,
+  Alert
+} from 'react-native';
+import { Avatar, Divider, Card } from 'react-native-elements';
 import { LinearGradient } from 'expo-linear-gradient';
 import { firebaseApp, ContextSet } from '../firebase';
 
@@ -14,46 +21,65 @@ const gray = '#f2f2f2';
 export default function InfoScreen(props) {
   const [auth, users, contents] = firebaseApp();
   const [data, setData] = useContext(ContextSet.DataContext);
+  const [likeList, setLikeList] = useState([]);
+
+  const checkLogout = () => {
+    Alert.alert(
+      '알림',
+      '정말 로그아웃 하시겠어요?',
+      [
+        {
+          text: '취소',
+          style: 'cancel'
+        },
+        { text: '확인', onPress: () => handleOnClickLogout() }
+      ],
+      { cancelable: false }
+    );
+  };
+
+  const handleOnClickLogout = () => {
+    auth
+      .signOut()
+      .then(() => {
+        setData(null);
+        setEmail('');
+        setPassword('');
+        Alert.alert('로그아웃 되었습니다.');
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   useEffect(() => {
     if (!data) {
       props.navigation.navigate('Auth');
     } else {
+      const tempLikeList = data['like'];
+      setLikeList(
+        tempLikeList.map((li, idx) =>
+          contents
+            .doc(li)
+            .get()
+            .then(doc => {
+              return (
+                <Text>{doc.data().title}</Text>
+                // <Card
+                //   containerStyle={styles.content}
+                //   image={{
+                //     uri:
+                //       'https://firebasestorage.googleapis.com/v0/b/media-e6082.appspot.com/o/photos%2Fphoto%2Fcat.jpg?alt=media&token=accdd002-422b-4fe5-b505-4b642cc5b780'
+                //   }}
+                // ></Card>
+              );
+            })
+        )
+      );
     }
   }, [data]);
 
-  // useEffect(() => {
-  //   contents
-  //     .doc(detailInfo)
-  //     .get()
-  //     .then(doc => {
-  //       rating.current = doc.data().rating;
-  //       count.current = doc.data().count;
-  //       setcontentInfo({
-  //         info: doc.data().info,
-  //         facilities: doc.data().facilities,
-  //         description: doc.data().description
-  //       });
-  //       contentTitle.current = doc.data().title;
-  //       const dataList = doc.data().comments;
-  //       setCommentList(
-  //         dataList
-  //           .map((info, idx) => {
-  //             console.log(info);
-  //             return (
-  //               <View key={idx}>
-  //                 <Rating imageSize={20} readonly startingValue={info.rating} />
-  //                 <Text>{info.id}</Text>
-  //                 <Text>{info.comment}</Text>
-  //               </View>
-  //             );
-  //           })
-  //           .reverse()
-  //       );
-  //     });
-  // }, []);
-
-  return (
+  return data ? (
     <View style={styles.container}>
       <LinearGradient
         start={{ x: 0, y: 0.5 }}
@@ -62,11 +88,14 @@ export default function InfoScreen(props) {
         style={{ flex: 1 }}
       ></LinearGradient>
       <View style={{ flex: 7 }}>
-        <View
-          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-        >
-          <Avatar size="large" rounded icon={{ name: 'person' }} />
-          <Text>{data['userName']}님</Text>
+        <View style={{ flex: 1 }}>
+          <TouchableHighlight onPress={checkLogout} underlayColor="white">
+            <Text style={{ textAlign: 'right' }}>로그아웃</Text>
+          </TouchableHighlight>
+          <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+            <Avatar size="large" rounded icon={{ name: 'person' }} />
+            <Text>{data['userName']}님</Text>
+          </View>
         </View>
         <View style={{ width, height: 20, backgroundColor: gray }}></View>
         <View style={{ flex: 1 }}>
@@ -103,15 +132,22 @@ export default function InfoScreen(props) {
           <Text>즐겨찾기</Text>
           <Divider style={{ height: 3, backgroundColor: gray }} />
         </View>
+        {/* <View>{likeList}</View> */}
       </View>
-      {/* <LogoutScreen /> */}
     </View>
+  ) : (
+    <View></View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1
+  },
+  content: {
+    width: 100,
+    borderRadius: 30,
+    overflow: 'hidden'
   }
 });
 
